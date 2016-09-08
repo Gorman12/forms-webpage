402fcd8528c0e3d41138cee024cfdec97340eb3a
sean gorman and tim devlin <tim@>
2016-09-07 21:19:07 -0700

    add function to replace text on the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 $(function() {
   $("#blanks form").submit(function(event){
-    $(".person1").append("stuff");
-    $(".person2").append("stuff");
-    $(".dance").append("stuff");
-    $(".week").append("stuff");
-    $(".describe").append("stuff");
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var danceInput = $("input#dance").val();
+    var weekInput = $("input#week").val();
+    var describeInput = $("input#describe").val();
+
+
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".dance").text(danceInput);
+    $(".week").text(weekInput);
+    $(".describe").text(describeInput);
 
     $("#story").show();
 
